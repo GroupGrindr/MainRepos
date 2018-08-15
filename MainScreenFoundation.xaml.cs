960781f4abc79cc376f2f8a8c3aef9694e67acb1
1015810 <38157340+1015810@users.noreply.github.com>
2018-08-15 11:12:31 +0800

    Add files via upload
    MainScreenFoundation lacks our profile photo. Only foundations. MainWindow directs to MainScreenFoundation as a NavigationWindow.

---
+++ MainScreenFoundation.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GroupGrindr
+{
+    /// <summary>
+    /// Interaction logic for MainScreenFoundation.xaml
+    /// </summary>
+    /// 
+    public class GlobalVariables
+    {
+
+    }
+    public partial class MainScreenFoundation : Page
+    {
+
+        public MainScreenFoundation()
+        {
+            InitializeComponent();
+        }
+
+        private void MyGroups_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateGroup_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Profile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
